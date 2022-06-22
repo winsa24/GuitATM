d2fcb66dc050a1a902ae557bbece782ded31d354
winsa24 <winsaaa24@gmail.com>
2022-06-22 11:21:17 +0200

    add deposit and withdraw bool

--- ATM.cpp
+++ ATM.cpp
@@ -48,49 +48,115 @@ ATM::ATM() : GWindow("#code-edit") {
     << ".clock .buttonbox {size: auto 7ch; align: center stretch}"
     << ".label .buttonbox .set-btn {size:6ch; font:bold}";
 
-    auto userPsw = "13579";
-    
-    auto mesg = Label("Welcom");
-    auto psw = Text();
-//    char* psw = "";
+    auto userPsw = Text("13579");
+
+    auto login = Bool(false);
     auto hasCard = Bool(false);
-    auto screen = Box("#screen")
+    auto depos = Bool(false);
+    auto withd = Bool(false);
+    
+    auto mesg = Label("Welcome");
+    auto psw = Text();
+    auto err = Text();
+    auto sideTextL = VBox();
+    auto sideTextR = VBox();
+    
+    auto screen = Box("#screen {flow: column; align: stretch stretch;}")
         << mesg
-        << psw;
+        << psw
+        << (Label() << err)
+        << (HBox("{flow: row; align: stretch end;}")
+            << sideTextL
+            << Box()
+            << sideTextR
+            );
+   
     
     auto bg = Background("pink");
     auto bd = Border("rounded");
     
-    auto en = Enabled(true);
+    auto en = Enabled(false);
     
     auto wrongCount = Int(0);
+    auto cashBox = Box();
+    auto cashCount = Int();
+    
+    auto interactMoney = [=](auto e){
+        
+    };
+    auto cancelBtn = [=](auto e){
+        sideTextL->removeChildren();
+        sideTextR->removeChildren();
+        sideTextL << "Deposit";
+        sideTextR << "Withdraw";
+        *depos = false;
+        *withd = false;
+    };
+    auto confirmBtn = [=](auto e){
+        *en = true;
+        sideTextL->removeChildren();
+        sideTextR->removeChildren();
+        sideTextL << "Exit";
+        sideTextR << "Continue";
+    };
+    auto depositMoney = [=](auto){
+        if(*depos == false){
+            *depos = true;
+            *mesg = "deposit";
+            cashBox << bg << bd << interactMoney;
+            sideTextL->removeChildren();
+            sideTextR->removeChildren();
+            sideTextL << "Cancel";
+            sideTextR << "Confirm";
+        }
+    };
+    auto withdrawMoney = [=](auto e){
+        *withd = true;
+        screen->remove(psw);
+        *mesg = "Select the amount of money";
+        sideTextL->removeChildren();
+        sideTextR->removeChildren();
+        sideTextL << "10" << "20" << "50" << "Cancel";
+        sideTextR << "100" << "300" << "Others" << "";
+    };
+    
     
   // main box
   *this
     << QuitDialog("Quit","Changes will be lost", &changed)
-    << (VBox(".window")
+    << (VBox(".window {align: stretch stretch;}")
 //        << On(~hasCard == false)  / (*en = true)
-        << On(~wrongCount > 3)  / (*en = true)
+        << On(~wrongCount > 3)  / [en] {*en = false;}
         << Label(".label ATM ")
         << (HBox()
             << (VBox()
                 << (HBox("{align: stretch end;}")
-                    << (VBox() << Button("") << Button("") << Button() << Button())
+                    << (VBox()
+                        << On(~login == true) / Enabled(true)
+                        << On(~login == false) / Enabled(false)
+                        << (Button()<< depositMoney) << Button() << Button()
+                        << (Button() << Guard(~depos == true)/cancelBtn) //TODO::
+                        )
                     << screen
-                    << (VBox() << Button("") << Button("") << Button() << Button())
+                    << (VBox()
+                        << On(~login == true) / Enabled(true)
+                        << On(~login == false) / Enabled(false)
+                        << (Button("{#withdraw}") << withdrawMoney) << Button("") << Button()
+                        << (Button("") << On(~depos == true || ~withd == true)/confirmBtn)) //TODO::
+                        )
+                << (VBox()
+                    << Label(".label Cash")
+                    << cashBox
                     )
-                << HBox() << Label(".label Cash")
                 )
             << (VBox()
                 << (Button(".label RECEIPT")
+                    << en
                     << On.click / [=](GMouseEvent* e){ *mesg = "Print RECEIPT"; }
                     )
-                << (Button(".label CARD") << en
-                    << On(~hasCard == true)  / (*en = false)
-                    << On(~hasCard == false) / (*en = true)
+                << (Button(".label CARD")
                     << On.click
-                    / [=](GMouseEvent* e){ *hasCard = true;}
-                    / [=]{ *mesg = "<color=green> Card Inserted"; }
+                    / [=]{ *hasCard = true; *mesg = "<color=green> Card Inserted"; }
 
                     // waits for 1 second whitout blocking the event loop
                     / SleepFor(2000)
@@ -101,38 +167,51 @@ ATM::ATM() : GWindow("#code-edit") {
                    )
                 )
             )
-//        << (HBox("#keyboard")
-//          << (VBox(".nobtn") << Button("1") << Button("4") << Button("7") << Button("*"))
-//          << (VBox(".nobtn") << Button("2") << Button("5") << Button("8") << Button("0"))
-//          << (VBox(".nobtn") << Button("3") << Button("6") << Button("9") << Button("#"))
-//          << (VBox() << Button("CANCEL") << Button("CLEAR") << Button("ENTER") << Button())
-//        )
         << (HBox("#keyboard {align: stretch stretch;}")
             << (Box("#noboard {flow: grid 3}")
-                << [=](auto e){ *psw += e->child()->textValue();}
+                << [=](auto e){ if(e->child()->textValue() != "*" && e->child()->textValue() != "\\#" )*psw += e->child()->textValue();}
+//                << Guard(~auth == true)   / Enabled(true)
+//                << Guard(~idle == true)   / Enabled(false)
+                << On(~hasCard == true) / Enabled(true)
+                << On(~hasCard == false) / Enabled(false)
                 << Button("1") << Button("2") << Button("3")
                 << Button("4") << Button("5") << Button("6")
                 << Button("7") << Button("8") << Button("9")
-                << Button("*") << Button("0") << Button("#")
+                << Button("*") << Button("0") << (Button("\\#") << []{})
                 )
             << (Box("#funboard {flow: grid 1}")
+                << On(~hasCard == true) / Enabled(true)
+                << On(~hasCard == false) / Enabled(false)
                 << (Button("CANCEL") << Background("yellow") << [=](auto e){ psw->erase(psw->stringValue().size() - 1, 1); })
                 << (Button("CLEAR") << bg << [=](auto e){ *psw = ""; })
                 << (Button("ENTER") << Background("green")
                     << [=](auto e){
-                            if(psw->stringValue() == userPsw) { *mesg = "<color=green> Login Successfully"; }
+                            if(psw->stringValue() == userPsw->stringValue()) {
+                                *mesg = "<color=green> Login Successfully";
+//                                *mesg = "<color=blue> Select Operation";
+                                screen->removeChildren();
+                                screen << mesg
+                                    << "select operation"
+                                    << (HBox("{flow: row; align: stretch end;}")
+                                        << (sideTextL << "Deposit" <<"" <<"" <<"")
+                                        << Box()
+                                        << (sideTextR << "Withdraw" <<"" <<"" <<"" )
+                                        );
+                                *login = true;
+                            }
                             else { ++ wrongCount;
                                 *mesg = "<color=red> Wrong Password";
-                                screen << "Left " + (4 - ~wrongCount) + " Attempts";
+                                *err = "Left " + to_string(4 - *wrongCount) + " Attempts";
                             }
-                        })
+                            *psw = "";
+                        }
+                    )
                 << Button("")
                 )
             )
         
     );
-  
-  makeInternal();   // disable inspection
+    makeInternal();   // disable inspection
 }
 
 
@@ -144,4 +223,3 @@ int main(int argc, const char** argv)
 
   return app.start(new ATM);
 }
-
